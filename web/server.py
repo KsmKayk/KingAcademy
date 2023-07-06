656171cbad159bae7e2b903adfd00cded471cb55
Kayk Mascarenhas <kaykdsg@gmail.com>
2023-07-06 17:48:10 -0300

    feat: Criação do Header no index.html e Instalação lib Gravatar

--- web/server.py
+++ web/server.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from src.routes import router
+from flask_gravatar import Gravatar
 
 # Cria uma instância do Flask
 app = Flask(__name__)
 
 # Registra as rotas do arquivo routes.py
+app.secret_key = "secret_key"
 app.register_blueprint(router)
+
+gravatar = Gravatar(
+    app,
+    size=100,
+    rating="g",
+    default="retro",
+    force_default=False,
+    use_ssl=False,
+    base_url=None,
+)
 
 
 # Verifica se o arquivo está sendo executado diretamente antes de executar o servidor
--- web/src/routes.py
+++ web/src/routes.py
@@ -1,5 +1,5 @@
 from os import path
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, url_for, session
 
 views_dir = path.abspath("./src/views")
 router = Blueprint("router", __name__, template_folder=views_dir)
@@ -7,4 +7,21 @@ router = Blueprint("router", __name__, template_folder=views_dir)
 
 @router.route("/")
 def index():
-    return render_template("home.html")
+    if "signed" in session:
+        return render_template(
+            "home.html", signed=True, user_email="teste@gmail.com", user_name="John Doe"
+        )
+    else:
+        return render_template("home.html", signed=False)
+
+
+@router.route("/signout", methods=["POST"])
+def signout():
+    session.pop("signed", None)
+    return redirect(url_for("router.index"))
+
+
+@router.route("/signin")
+def signin():
+    session["signed"] = True
+    return redirect(url_for("router.index"))
